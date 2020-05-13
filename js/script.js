6d1b8bea7fd7274872f4a79a1bb390a6a12066ac
Sue <imsomeonelse@gmail.com>
2020-05-12 18:31:56 -0700

    Link functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,117 @@
 $(() => {
+    let _$linkAbout = '';
+    let _$linkExperience = '';
+    let _$linkSkills = '';
+    let _$linkProjects = '';
+    let _$linkContact = '';
 
+    let _$sectionAbout = '';
+    let _$sectionExperience = '';
+    let _$sectionSkills = '';
+    let _$sectionProjects = '';
+    let _$sectionContact = '';
+
+    let cacheDom = () => {
+        _$linkAbout = $('#linkAbout');
+        _$linkExperience = $('#linkExp');
+        _$linkSkills = $('#linkSkills');
+        _$linkProjects = $('#linkProjects');
+        _$linkContact = $('#linkContact');
+
+        _$sectionAbout = $('#overlay');
+        _$sectionExperience = $('#experience');
+        _$sectionSkills = $('#skills');
+        _$sectionProjects = $('#projects');
+        _$sectionContact = $('#contact');
+    }
+
+    let fx = () => {
+        _$linkAbout.click(()=>{
+            openAbout();
+        });
+        _$linkExperience.click(()=>{
+            openExperience();
+        });
+        _$linkSkills.click(()=>{
+            openSkills();
+        });
+        _$linkProjects.click(()=>{
+            openProjects();
+        });
+        _$linkContact.click(()=>{
+            openContact();
+        });
+    }
+
+    let closeAbout = () => {
+        _$sectionAbout.fadeOut();
+    }
+
+    let closeExperience = () => {
+        _$sectionExperience.fadeOut();
+    }
+
+    let closeSkills = () => {
+        _$sectionSkills.fadeOut();
+    }
+
+    let closeProjects = () => {
+        _$sectionProjects.fadeOut();
+    }
+
+    let closeContact = () => {
+        _$sectionContact.fadeOut();
+    }
+
+    let openAbout = () => {
+        closeExperience();
+        closeSkills();
+        closeProjects();
+        closeContact();
+
+        _$sectionAbout.fadeIn();
+    }
+
+    let openExperience = () => {
+        closeAbout();
+        closeSkills();
+        closeProjects();
+        closeContact();
+
+        _$sectionExperience.fadeIn();
+    }
+
+    let openSkills = () => {
+        closeExperience();
+        closeAbout();
+        closeProjects();
+        closeContact();
+
+        _$sectionSkills.fadeIn();
+    }
+
+    let openProjects = () => {
+        closeExperience();
+        closeSkills();
+        closeAbout();
+        closeContact();
+
+        _$sectionProjects.fadeIn();
+    }
+
+    let openContact = () => {
+        closeExperience();
+        closeSkills();
+        closeProjects();
+        closeAbout();
+
+        _$sectionContact.fadeIn();
+    }
+    
+    let init = () => {
+        cacheDom();
+        fx();
+    }
+
+    init();
 });
